b40ba3182879c9069e2c16d8aa272bd1cdcfcd7a
popovichppravi <popovichppravi@gmail.com>
2019-01-05 20:13:40 +0300

    begin template

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Fonts CSS -->
+    <link rel="stylesheet" href="css/fonts.css" >
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+
+    <!--Удалить-->
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    <!--//Удалить-->
+  </head>
+  <body>
+    <!--HEADER-->
+    <div class="header">
+
+    </div>
+    <!--//HEADER-->
+    
+    <!--PAGE CONTENT-->
+    <div class="page-content">
+      <h1>Hello, world!</h1>      
+    </div>
+    <!--PAGE CONTENT-->
+
+    <!--FOOTER-->
+    <div class="footer">
+
+    </div>
+    <!--//FOOTER-->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
